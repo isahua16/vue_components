1fae1705d19cca0642de099a14befd98e8e31496
isahua16 <isaelhuard@gmail.com>
2023-04-13 19:33:59 -0600

    my first vue component

---
+++ src/components/PageHeader.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+        <h1>LOGO</h1>
+        <nav>
+            <a href="">About</a>
+            <a href="">Store</a>
+        </nav>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    div {
+        display: grid;
+        place-items: center;
+        grid-template-columns: 1fr 1fr;
+    }
+
+    nav {
+        display: grid;
+        grid-auto-flow: column;
+        place-items: center;
+        column-gap: 12px;
+    }
+
+    a {
+        text-decoration: none;
+        color: black;
+    }
+
+</style>
